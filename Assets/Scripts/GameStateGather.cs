f0748cee819783c6fa3050eee2ad23065f010bcb
siolaterr <Brendan@Brendans-MacBook-Pro.local>
2016-03-08 00:01:23 -0800

    Made gather mode/GPS display number of resources.

--- Assets/Scripts/GameStateGather.cs
+++ Assets/Scripts/GameStateGather.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class TestLocationService : MonoBehaviour
-{
+public class GameStateGather : MonoBehaviour {
 	Text GPS_dest;
 	Text GPS_curr;
 	RawImage brick;
@@ -15,10 +14,14 @@ public class TestLocationService : MonoBehaviour
 	float longitude = Random.Range(8F, 10F);
 	float curr_lat;
 	float curr_long;
-	float range = .0001f;
+	float range = 0.0001f;
+	bool collected = false;
 
-	IEnumerator Start()
-	{
+	IEnumerator Start () {
+		// Hacky workaround fix
+		SaveManager.GameNew();
+		SaveManager.GameDataSave.printData();
+
 		GPS_dest = GameObject.Find("GPS_dest").GetComponent<Text>();
 		GPS_curr = GameObject.Find("GPS_curr").GetComponent<Text>();
 
@@ -31,7 +34,6 @@ public class TestLocationService : MonoBehaviour
 		rock.enabled = false;
 		log.enabled = false;
 		collect.SetActive(false);
-
 
 		// First, check if user has location service enabled
 		if (!Input.location.isEnabledByUser) {
@@ -46,32 +48,24 @@ public class TestLocationService : MonoBehaviour
 
 		// Wait until service initializes
 		int maxWait = 20;
-		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-		{
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
 			yield return new WaitForSeconds(1);
 			maxWait--;
 		}
 
 		// Service didn't initialize in 20 seconds
-		if (maxWait < 1)
-		{
-			
+		if (maxWait < 1) {
 			GPS_dest.text = "Timed out";
 			GPS_curr.text = "Timed out";
 			yield break;
 		}
 
 		// Connection has failed
-		if (Input.location.status == LocationServiceStatus.Failed)
-		{
+		if (Input.location.status == LocationServiceStatus.Failed) {
 			GPS_dest.text = "Unable to determine device location";
 			GPS_curr.text = "Unable to determine device location";
-
 			yield break;
-		}
-		else
-		{
-			
+		} else {
 			lat /= 100000;
 			longitude /= 10000000;
 			lat = lat + Input.location.lastData.latitude;
@@ -82,14 +76,10 @@ public class TestLocationService : MonoBehaviour
 
 			GPS_curr.text = "Current Location: " + curr_lat + " " + curr_long;
 			GPS_dest.text = "Target Location: " + lat + " " + longitude;
-
-
 		}
-
-
+			
 		// Stop service if there is no need to query location updates continuously
 		//Input.location.Stop();
-
 	}
 
 
@@ -99,30 +89,43 @@ public class TestLocationService : MonoBehaviour
 			GPS_curr.text = "Unable to determine device location";
 			yield break;
 		} else {
-			
-				
 			curr_lat = Input.location.lastData.latitude;
 			curr_long = Input.location.lastData.longitude;
-
-			GPS_curr.text = "Current Location: " + curr_lat + " " + curr_long;
-
-			if ((curr_lat < lat + range && curr_lat > lat - range) && (curr_long < longitude + range && curr_long > longitude - range)) {
-				GPS_curr.text = "YOU MADE IT!!!!!!";
-				if (item == 1)
-					brick.enabled = true;
-				else if (item == 2)
-					rock.enabled = true;
-				else
-					log.enabled = true;
-				collect.SetActive(true);
-			} else {
-				brick.enabled = false;
-				rock.enabled = false;
-				log.enabled = false;
-				collect.SetActive(false);
+			if (!collected) {
+				GPS_curr.text = "Current Location: " + curr_lat + " " + curr_long;
+				if ((curr_lat < lat + range && curr_lat > lat - range) && (curr_long < longitude + range && curr_long > longitude - range)) {
+					GPS_curr.text = "YOU MADE IT!!!!!!";
+					collected = true;
+					if (item == 1)
+						brick.enabled = true;
+					else if (item == 2)
+						rock.enabled = true;
+					else
+						log.enabled = true;
+					collect.SetActive (true);
+				} else {
+					brick.enabled = false;
+					rock.enabled = false;
+					log.enabled = false;
+					collect.SetActive (false);
+				}
 			}
 		}
 	}
+
+	public void collectItem () {
+		if (item == 1) {
+			SaveManager.GameDataSave.numBrick++;
+			GPS_curr.text = SaveManager.GameDataSave.numBrick.ToString();
+		} else if (item == 2) {
+			SaveManager.GameDataSave.numOre++;
+			GPS_curr.text = SaveManager.GameDataSave.numOre.ToString();
+		} else {
+			SaveManager.GameDataSave.numWood++;
+			GPS_curr.text = SaveManager.GameDataSave.numWood.ToString();
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		StartCoroutine(Location());
